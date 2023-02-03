fa34a4bca723665dfb57bf6c0693f72b02c4e906
Zhaosi Qu <git@chaos.run>
2023-02-03 06:02:15 +0100

    Remove some endpoints from public access

--- core/main.py
+++ core/main.py
@@ -1,42 +1,23 @@
 import logging
-from typing import Union
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 
-from core.routers import event_log, dashboard, event, case, result
+from core.routers import event_log
 
 # Enable logging
 logger = logging.getLogger(__name__)
 
 # Create the app
 app = FastAPI()
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost:5173",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 app.include_router(event_log.router)
-app.include_router(dashboard.router)
-app.include_router(event.router)
-app.include_router(case.router)
-app.include_router(result.router)
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
 
 
 @app.get("/")
