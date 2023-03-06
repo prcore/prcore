aa91aa7292fc994530468e7cad032743ff903047
Zhaosi Qu <git@chaos.run>
2023-03-07 01:45:07 +0200

    Only remove builder cache

--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -13,7 +13,7 @@ sudo rm -rf ./data
 cd ./scripts || exit
 bash mkdir.sh
 cd ..
-docker system prune -f --filter "until=6h"
+docker builder prune -f --filter "until=6h"
 docker compose build
 docker compose up -d
 
