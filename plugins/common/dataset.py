4bb57372f4bd36aeba1e23fc304c03e8f2baf5c6
Zhaosi Qu <git@chaos.run>
2023-03-13 22:33:15 +0200

    Add helper functions to get training dataset by activity for each length

--- plugins/common/dataset.py
+++ plugins/common/dataset.py
@@ -15,13 +15,18 @@ from core.functions.general.etc import get_processes_number
 logger = logging.getLogger(__name__)
 
 
-def get_encoded_dataframes_by_activity(original_df: DataFrame, encoding_type: EncodingType, outcome_type: OutcomeType,
-                                       include_treatment: bool) -> Tuple[Dict[int, DataFrame], Dict[str, Any]]:
+def get_encoded_dfs_by_activity(original_df: DataFrame, encoding_type: EncodingType, outcome_type: OutcomeType,
+                                include_treatment: bool, for_test: bool,
+                                existing_data: Optional[Dict[str, Any]]) -> Tuple[Dict[int, DataFrame], Dict[str, Any]]:
     # Get encoded dataframes by activity for different lengths
     df = original_df.copy(deep=True)
     data = {}
 
-    if encoding_type == EncodingType.SIMPLE_INDEX:
+    if existing_data is not None and "mapping" in existing_data:
+        mapping = existing_data["mapping"]
+        df = get_ordinal_encoded_df_by_given_mapping(df, ColumnDefinition.ACTIVITY, mapping)
+        data["mapping"] = mapping
+    elif encoding_type == EncodingType.SIMPLE_INDEX:
         df, mapping = get_ordinal_encoded_df_and_mapping(df, ColumnDefinition.ACTIVITY)
         data["mapping"] = mapping
     elif outcome_type == OutcomeType.LAST_ACTIVITY:
@@ -40,8 +45,11 @@ def get_encoded_dataframes_by_activity(original_df: DataFrame, encoding_type: En
         treatments = df[ColumnDefinition.TREATMENT].values
         data["grouped_treatments"] = [treatments[case_ids == case_id][0] for case_id in unique_case_ids]
     if encoding_type in {EncodingType.BOOLEAN, EncodingType.FREQUENCY_BASED}:
-        lb = LabelBinarizer()
-        lb = lb.fit(np.unique(activities))
+        if existing_data is not None and "lb" in existing_data:
+            lb = existing_data["lb"]
+        else:
+            lb = LabelBinarizer()
+            lb = lb.fit(np.unique(activities))
         data["lb"] = lb
 
     processes_number = get_processes_number()
@@ -49,11 +57,12 @@ def get_encoded_dataframes_by_activity(original_df: DataFrame, encoding_type: En
     with Pool(processes=processes_number) as pool:
         results = pool.starmap(
             func=get_encoded_dataframes_by_activity_for_lengths,
-            iterable=[(lengths, encoding_type, outcome_type, data) for lengths in lengths_split]
+            iterable=[(lengths, encoding_type, outcome_type, data, for_test) for lengths in lengths_split]
         )
-    results = {length: result for result in results for length, result in result.items()}
 
-    return results, data
+    dataframes = {length: r for result in results for length, r in result.items()}
+
+    return dataframes, data
 
 
 def get_ordinal_encoded_df_and_mapping(original_df: DataFrame, column: str) -> Tuple[DataFrame, dict]:
@@ -66,8 +75,8 @@ def get_ordinal_encoded_df_and_mapping(original_df: DataFrame, column: str) -> T
     return df, mapping
 
 
-def get_ordinal_encoded_df_by_given_mapping(original_df: DataFrame, mapping: Dict[Union[int, str], int],
-                                            column: str) -> DataFrame:
+def get_ordinal_encoded_df_by_given_mapping(original_df: DataFrame, column: str,
+                                            mapping: Dict[Union[int, str], int]) -> DataFrame:
     # Get ordinal encoded dataframe
     df = original_df.copy(deep=True)
     df[column] = df[column].map(mapping)
@@ -77,43 +86,37 @@ def get_ordinal_encoded_df_by_given_mapping(original_df: DataFrame, mapping: Dic
 
 def get_encoded_dataframes_by_activity_for_lengths(lengths: List[int], encoding_type: EncodingType,
                                                    outcome_type: OutcomeType,
-                                                   data: Dict[str, Any]) -> Dict[int, DataFrame]:
+                                                   data: Dict[str, Any], for_test: bool) -> Dict[int, DataFrame]:
     # Get encoded dataframes by activity for some lengths
     results = {}
     for length in lengths:
-        if len([group for group in data["grouped_activities"] if len(group) > length]) < 1000:
+        if not for_test and len([group for group in data["grouped_activities"] if len(group) > length]) < 1000:
             continue
-        training_df = get_training_df_by_activity(length, encoding_type, outcome_type, data)
-        if training_df is None:
+        if for_test:
+            encoded_df = get_test_df_by_activity(length, encoding_type, data)
+        else:
+            encoded_df = get_training_df_by_activity(length, encoding_type, outcome_type, data)
+        if encoded_df is None:
             continue
-        results[length] = training_df
+        results[length] = encoded_df
     return results
 
 
-def get_training_df_by_activity(length: int, encoding_type: EncodingType, outcome_type: OutcomeType,
-                                data: Dict[str, Any]) -> Optional[DataFrame]:
-    training_data = []
+def get_test_df_by_activity(length: int, encoding_type: EncodingType, data: Dict[str, Any]) -> Optional[DataFrame]:
+    test_data = []
     for i, group in enumerate(data["grouped_activities"]):
-        if len(group) <= length:
+        if len(group) != length:
             continue
-        x = get_training_array(group, length, encoding_type, data.get("lb"))
-        y = get_outcome_label_by_activity(group, data.get("grouped_outcomes"), length, i, outcome_type)
-        t = get_treatment_label(data.get("grouped_treatments"), i)
-        if x is None or y is None:
+        x = get_data_array(group, length, encoding_type, data.get("lb"))
+        if x is None:
             continue
-        if data.get("grouped_treatments") is not None:
-            training_data.append([x, y, t])
-        else:
-            training_data.append([x, y])
-    training_df = get_training_df_from_data_list(training_data, length, encoding_type, data.get("lb"))
-    if training_df is not None and outcome_type == OutcomeType.LAST_ACTIVITY:
-        training_df = get_ordinal_encoded_df_by_given_mapping(training_df, data["mapping"], ColumnDefinition.OUTCOME)
-    return training_df
+        test_data.append([x])
+    return get_test_df_from_data_list(test_data, length, encoding_type, data.get("lb"))
 
 
-def get_training_array(group: np.array, length: int, encoding_type: EncodingType,
-                       lb: Optional[LabelBinarizer]) -> Optional[np.ndarray]:
-    # Get training array for a specific length
+def get_data_array(group: np.array, length: int, encoding_type: EncodingType,
+                   lb: Optional[LabelBinarizer]) -> Optional[np.ndarray]:
+    # Get data array for a specific length
     x_raw = group[:length]
     if encoding_type == EncodingType.BOOLEAN and lb is not None:
         arr_encoded = lb.transform(x_raw)
@@ -127,6 +130,50 @@ def get_training_array(group: np.array, length: int, encoding_type: EncodingType
     else:
         x = None
     return x
+
+
+def get_test_df_from_data_list(test_data: List[List[Union[np.array, int]]], length: int, encoding_type: EncodingType,
+                               lb: Optional[LabelBinarizer]) -> Optional[DataFrame]:
+    if len(test_data) == 0:
+        return None
+    df = pd.DataFrame(data=test_data, columns=[ColumnDefinition.ACTIVITY])
+    return get_activities_df(df, length, encoding_type, lb)
+
+
+def get_activities_df(df: DataFrame, length: int, encoding_type: EncodingType,
+                      lb: Optional[LabelBinarizer]) -> DataFrame:
+    if encoding_type in {EncodingType.BOOLEAN, EncodingType.FREQUENCY_BASED} and lb is not None:
+        activity_columns = lb.classes_
+    else:
+        activity_columns = [f"EVENT_{i}" for i in range(1, length + 1)]
+    activities_df = pd.DataFrame(
+        data=np.stack(df[ColumnDefinition.ACTIVITY], axis=0),
+        columns=activity_columns
+    )
+    return activities_df
+
+
+def get_training_df_by_activity(length: int, encoding_type: EncodingType, outcome_type: OutcomeType,
+                                data: Dict[str, Any]) -> Optional[DataFrame]:
+    training_data = []
+    for i, group in enumerate(data["grouped_activities"]):
+        if len(group) <= length:
+            continue
+        x = get_data_array(group, length, encoding_type, data.get("lb"))
+        y = get_outcome_label_by_activity(group, data.get("grouped_outcomes"), length, i, outcome_type)
+        t = get_treatment_label(data.get("grouped_treatments"), i)
+        if x is None or y is None:
+            continue
+        if data.get("grouped_treatments") is not None:
+            training_data.append([x, y, t])
+        else:
+            training_data.append([x, y])
+    training_df = get_training_df_from_data_list(training_data, length, encoding_type, data.get("lb"))
+    if (training_df is not None
+            and encoding_type in {EncodingType.BOOLEAN, EncodingType.FREQUENCY_BASED}
+            and outcome_type == OutcomeType.LAST_ACTIVITY):
+        training_df = get_ordinal_encoded_df_by_given_mapping(training_df, ColumnDefinition.OUTCOME, data["mapping"])
+    return training_df
 
 
 def get_outcome_label_by_activity(activities: np.array, grouped_outcomes: Optional[List[int]], length: int, index: int,
@@ -161,15 +208,7 @@ def get_training_df_from_data_list(training_data: List[List[Union[np.array, int]
         columns = [ColumnDefinition.ACTIVITY, ColumnDefinition.OUTCOME, ColumnDefinition.TREATMENT]
     training_df = pd.DataFrame(data=training_data, columns=columns)
 
-    if encoding_type in {EncodingType.BOOLEAN, EncodingType.FREQUENCY_BASED} and lb is not None:
-        activity_columns = lb.classes_
-    else:
-        activity_columns = [f"EVENT_{i}" for i in range(1, length + 1)]
-    activities_df = pd.DataFrame(
-        data=np.stack(training_df[ColumnDefinition.ACTIVITY], axis=0),
-        columns=activity_columns
-    )
-
+    activities_df = get_activities_df(training_df, length, encoding_type, lb)
     if len(training_data[0]) == 2:
         label_columns = [ColumnDefinition.OUTCOME]
     else:
