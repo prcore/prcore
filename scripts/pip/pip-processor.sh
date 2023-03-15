464b6be31bd6eeb91c46d649fcd8048400d6bf9a
Zhaosi Qu <git@chaos.run>
2023-03-15 20:40:32 +0200

    Update dependencies

--- scripts/pip/pip-processor.sh
+++ scripts/pip/pip-processor.sh
@@ -3,7 +3,7 @@
 pip freeze | grep -v "^-e" | xargs pip uninstall -y
 pip install -U pip
 pip install -U setuptools wheel
-pip install -U APScheduler pandas pika pika-stubs scikit-learn
+pip install -U APScheduler pandas pika pika-stubs pydantic scikit-learn
 pip freeze > requirements.txt
 sed "/^pkg-resources==0.0.0$/d" requirements.txt > ../../processor/requirements.txt
 rm requirements.txt
